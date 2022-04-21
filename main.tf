250bbf266fe287f97451745a9f43a2cccff96997
Josh Gubler <josh.gubler@gmail.com>
2022-04-21 10:33:31 -0700

    refactor for_each to simplify

--- main.tf
+++ main.tf
@@ -31,19 +31,12 @@ resource "aws_acm_certificate_validation" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
+  for_each = aws_acm_certificate.cert.domain_validation_options
   provider = aws.aws_n_va
-  name     = each.value.name
-  type     = each.value.type
+  name     = each.resource_record_name
+  type     = each.resource_record_type
   zone_id  = var.hosted_zone_id
-  records  = [each.value.record]
+  records  = [each.resource_record_value]
   ttl      = 60
 }
 
