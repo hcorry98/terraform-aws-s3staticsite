f170e2b886fb51228bb532fb5dd8b7ca5adb5840
Jamie Visker <jamievisker@gmail.com>
2023-01-04 15:39:10 -0700

    empty value

--- main.tf
+++ main.tf
@@ -55,9 +55,9 @@ resource "aws_cloudfront_origin_access_control" "oac" {
   signing_protocol                  = "sigv4"
 }
 
-resource "aws_cloudfront_origin_access_identity" "oac" {
-
-}
+#resource "aws_cloudfront_origin_access_identity" "oac" {
+#
+#}
 resource "aws_cloudfront_distribution" "cdn" {
   price_class = var.cloudfront_price_class
   origin {
@@ -67,7 +67,7 @@ resource "aws_cloudfront_distribution" "cdn" {
     origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
 
     s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.oac.cloudfront_access_identity_path
+      origin_access_identity = ""#aws_cloudfront_origin_access_identity.oac.cloudfront_access_identity_path
     }
   }
 
